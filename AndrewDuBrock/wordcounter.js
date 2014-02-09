34523120e1e27b374e6fd6eec223fd820a7e74f2
Andrew DuBrock <adubrock@comcast.net>
2014-02-09 12:01:42 -0800

    finished intermediate homework

---
+++ AndrewDuBrock/wordcounter.js
@@ -0,0 +1,18 @@
+//Intermediate Assignment
+
+var fs = require('fs');
+
+fs.readFile('../paradise.txt', {encoding: 'utf8'}, function (err, contents) {
+  if (err) throw err;
+  var paradiseArray = contents.split('\n');
+  var words = [];
+  paradiseArray.forEach(function (line) {
+    line = line.replace(/\s{2,}/, ' ').replace(/[,.:;!]/, '');
+    line = line.split(' ');
+    if (line[0] === '') {
+      line.shift();
+    }
+    words = words.concat(line);
+  })
+  console.log('There are ' + words.length + ' words in the first chapter of Paradise Lost.');
+});
