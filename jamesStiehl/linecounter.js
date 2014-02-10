16bc486457e66ca7dc9f979bec84ca8db2bf8a1a
James Stiehl <jstiehl@gmail.com>
2014-02-10 13:40:34 -0800

    fixed regex for wordcounter

--- jamesStiehl/linecounter.js
+++ jamesStiehl/linecounter.js
@@ -3,8 +3,8 @@ var fs = require('fs');
 var lines = fs.readFileSync('paradise.txt', {encoding: 'utf8'})
 var lineCount = 1;
 
-for (i=0;i<lines.length;i++){
-	if(lines[i]==='\n'){
+for (var i=0; i<lines.length; i++){
+	if(lines[i] === '\n'){
 		lineCount++;
 	}
 } 
--- jamesStiehl/wordcounter.js
+++ jamesStiehl/wordcounter.js
@@ -2,8 +2,7 @@ var fs = require('fs');
 
 var contents = fs.readFileSync('paradise.txt', {encoding: 'utf8'});
 
-var wordArray = contents.split(/\W+/);
-wordArray.pop();
+var wordArray = contents.split(/[^\w'-]+/);
 
 console.log("There are "+wordArray.length+" words in the paradise.txt file");
 
