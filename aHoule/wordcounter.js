2c277ab009f88f6198c2c9120be219cd53603729
amanda <ah@ahs-macbook-pro-2.local>
2014-02-11 12:59:53 -0800

    fixed basic and intermediate

--- aHoule/wordcounter.js
+++ aHoule/wordcounter.js
@@ -1,26 +1,18 @@
 var fs = require('fs');
 
-var content = fs.readFile('paradise.txt', {encoding: 'utf8'}); //, function (err, contents)
-
-/* This, I used as a test variable:
-var content = "Of Man's first disobedience, and the fruit\n" +
-"Of that forbidden tree whose mortal taste\n" +
-"Brought death into the World, and all our woe,\n" +
-"With loss of Eden, till one greater Man\n" +
-"Restore us, and regain the blissful Seat,"
-*/
+fs.readFile('paradise.txt', {encoding: 'utf8'}, function(err, contents) {
+	if (err) throw err;
+	var wordCount = 1;
 
     // count the number of words in content
 
-var wordCount = 1;
-
-for (var i = 0; i < content.length; i++)
-	// if there is a space or new line, add 1 to wordCount
-
-	if (content[i] === ' ' || content[i] === "\n") {
+	for (var i = 0; i < contents.length; i++)
+	if (contents[i] === ' ' || contents[i] === "\n") {
 		wordCount ++;
 };
 
 console.log("There are " + wordCount + " number of words in this text file.");
+});
+
 
 // This is returning an error:  "Cannot read property 'length' of undefined" referring "content.length"
--- linecounter.js
+++ linecounter.js
@@ -1,3 +1,4 @@
+/*
 var fs = require('fs');
 
 var content = fs.readFile('paradise.txt', {encoding: 'utf8'}); //, function (err, contents)
@@ -12,3 +13,24 @@ for (var i = 0; i < content.length; i++)
 };
 
 console.log("There are " + numOfLines + " number of lines in this text file.");
+*/
+// This is returning an error:  "Cannot read property 'length' of undefined" referring "content.length"
+
+
+//  Ok, I'm starting over but I don't want to erase what i was doing before yet.
+
+var fs = require('fs');
+
+//var numOfLines;
+
+fs.readFile('paradise.txt', 'utf8', function(err, contents) {
+	if (err) throw err;
+	numOfLines = 1;
+
+	for (var i = 0; i < contents.length; i++)
+		if (contents[i] === '\n') {
+			numOfLines ++;
+		};
+	console.log("There are " + numOfLines + " number of lines in this text file.");
+
+})
