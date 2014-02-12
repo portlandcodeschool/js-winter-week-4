aa26040858c64559928d5d847492314f066a65a7
trippel <cpeddecord@gmail.com>
2014-02-12 10:19:58 -0800

    fixed wordcounter.js for reals?

--- ChrisPedd/wordcounter.js
+++ ChrisPedd/wordcounter.js
@@ -3,7 +3,6 @@ var fs = require('fs');
 fs.readFile('paradise.txt', { encoding: 'utf8' }, function (err, paradise) {
 	if (err) throw err;
 	var count = 0;
-	//var paradiseArr = paradise.split(/[^\w]+/);
 	var paradiseArr = paradise.replace(/'/g, '').split(/[^\w'-]+/);
 	console.log(paradiseArr);
 	count = paradiseArr.length;
