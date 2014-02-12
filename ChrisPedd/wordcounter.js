544c1b16619f24e3f4dd2a085dab63c70031adf4
trippel <cpeddecord@gmail.com>
2014-02-12 10:18:17 -0800

    fixed wordscounter.js maybe?

--- ChrisPedd/wordcounter.js
+++ ChrisPedd/wordcounter.js
@@ -3,7 +3,9 @@ var fs = require('fs');
 fs.readFile('paradise.txt', { encoding: 'utf8' }, function (err, paradise) {
 	if (err) throw err;
 	var count = 0;
-	var paradiseArr = paradise.replace(/'/g, '').split(/\W+/);
-	count = paradiseArr.length -1;
-	console.log("Number of words = " +count)
+	//var paradiseArr = paradise.split(/[^\w]+/);
+	var paradiseArr = paradise.replace(/'/g, '').split(/[^\w'-]+/);
+	console.log(paradiseArr);
+	count = paradiseArr.length;
+	console.log("Number of words = " + (count - 1))
 })
